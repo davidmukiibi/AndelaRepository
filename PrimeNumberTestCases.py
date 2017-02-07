26e3b07f87c1fb70589f597a56f38c5b9df3cf76
davidmukiibi <david.mukiibiq@gmail.com>
2017-02-07 14:37:47 +0300

    this is the prime number problem test cases

---
+++ PrimeNumberTestCases.py
@@ -0,0 +1,12 @@
+class PrimeFunctionTest(TestCase):
+
+def testNegattive(self):
+  self.assertEqual(haha, haha<0, msg = "the number entered is a negative")
+
+
+def testInteger(self):
+  self.assertEqual(type(haha), not int, msg = "the value entered should be an integer")
+
+
+def testZero(self):
+  self.assertEqual(0, haha, msg = "number entered is zero, please entera non zero number")
