c92af8d39dc87885dfd95979d0170340e871de7f
davidmukiibi <david.mukiibiq@gmail.com>
2017-02-07 10:01:45 +0300

    this is the fourth commit

--- primeNumberFunction.py
+++ primeNumberFunction.py
@@ -6,3 +6,7 @@ def primefunction(number):
             elif number % 2 == 0 or number % 3 == 0:
                 return "The given number is not a prime number"
             else:
+                return "the given number is a prime number"
+        else:
+            return "the given number is zero or a negative number"
+
