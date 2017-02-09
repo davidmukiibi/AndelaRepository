09e9bf3c55f01e745754df7216768bab31b44f30
davidmukiibi <david.mukiibiq@gmail.com>
2017-02-09 19:46:59 +0300

    i have corrected the issue this program had, now it checks out well

--- primeNumberFunction.py
+++ primeNumberFunction.py
@@ -1,5 +1,5 @@
 def primefunction(number):
-    if isinstance(number, int()):
+    if isinstance(number, int):
         if number > 0:
             if number == 2 or number == 3:
                 return "the given number is a prime number"
